9f7f36c12ad4fc47b2a8854b4139a3bf706ccac2
FelipheJ777 <feliphe777oficial@gmail.com>
2020-11-07 13:47:55 -0300

    git add . git commit -m "save"

--- src/main/java/br/com/projeto/commons/StepExecutor.java
+++ src/main/java/br/com/projeto/commons/StepExecutor.java
@@ -1,12 +1,55 @@
 package br.com.projeto.commons;
 
 import java.lang.reflect.Method;
+import java.util.Objects;
 
 public class StepExecutor {
 
-    public static void execute(Method method, Object instance, Object... args) {
+    public static void execute(Object instance, Method method, Object... args) {
         try {
-            method.invoke(instance, args);
+            if (Objects.nonNull(args) && args.length > 0) {
+                method.invoke(instance, args);
+            } else {
+                method.invoke(instance);
+            }
+        } catch (Exception ex) {
+            // Caso ocorra alguma exceção do método chamado ou da reflexão
+        } catch (AssertionError ae) {
+            // Caso falhe a validação do step
+        } finally {
+            // Gerar evidencias
+        }
+    }
+
+    public static Method findMethod(String name, Object instance,  Object... args) {
+        Method m;
+
+        return m;
+    }
+
+    @Deprecated
+    public static void execute(Object instance, String methodName, Object... args) {
+        if (Objects.isNull(instance)) throw new NullPointerException("The instance cannot be null");
+        if (Objects.isNull(methodName) || Objects.equals(true, methodName.isEmpty())) throw new NullPointerException("The method name cannot be empty.");
+        int i = 0, c = 0, p;
+        try {
+            for (Method method : instance.getClass().getDeclaredMethods()) {
+                if (method.getName().equals(methodName)) {
+                    if (Objects.equals((p = method.getParameterCount()), args.length)) {
+                        for (Class clazz : method.getParameterTypes()) {
+                            if (Objects.equals(args[i].getClass().getName(), clazz.getName())) {
+                                c++;
+                            }
+                            i++;
+                            instance.getClass().getDeclaredMethod(methodName,)
+                        }
+                        if (Objects.equals(c, p)) {
+
+                        }
+                    }
+                }
+            }
+
         } catch (Exception ex) {
             // Caso ocorra alguma exceção do método chamado ou da reflexão
         } catch (AssertionError ae) {
