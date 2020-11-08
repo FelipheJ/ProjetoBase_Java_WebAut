62f9e31e30342b488afccd8cbcf314d7f221ad37
FelipheJ777 <feliphe777oficial@gmail.com>
2020-11-08 11:45:57 -0300

    Criação do método de busca por um método em uma instância de um objeto.

--- src/main/java/br/com/projeto/commons/StepExecutor.java
+++ src/main/java/br/com/projeto/commons/StepExecutor.java
@@ -1,7 +1,7 @@
 package br.com.projeto.commons;
 
+import java.util.*;
 import java.lang.reflect.Method;
-import java.util.Objects;
 
 public class StepExecutor {
 
@@ -21,41 +21,41 @@ public class StepExecutor {
         }
     }
 
-    public static Method findMethod(String name, Object instance,  Object... args) {
-        Method m;
+    /**
+     * This method gets the specified method of an object.
+     *
+     * @param name     - The name of the method.
+     * @param instance - The instance of object that contains the method.
+     * @param args     - The arguments of the method (if there is).
+     * @return the corresponding method or null if, and only if, method has not found.
+     */
+    public static Method findMethod(String name, Object instance, Object... args) {
+        Method m = null;
+        List<Method> methodList = new ArrayList<>();
 
-        return m;
-    }
+        if (Objects.isNull(instance)) throw new IllegalArgumentException("The object instance cannot be null.");
+        if (Objects.isNull(name) || Objects.equals(name.isEmpty(), true)) throw new IllegalArgumentException("The name cannot be null or empty.");
 
-    @Deprecated
-    public static void execute(Object instance, String methodName, Object... args) {
-        if (Objects.isNull(instance)) throw new NullPointerException("The instance cannot be null");
-        if (Objects.isNull(methodName) || Objects.equals(true, methodName.isEmpty())) throw new NullPointerException("The method name cannot be empty.");
-        int i = 0, c = 0, p;
-        try {
-            for (Method method : instance.getClass().getDeclaredMethods()) {
-                if (method.getName().equals(methodName)) {
-                    if (Objects.equals((p = method.getParameterCount()), args.length)) {
-                        for (Class clazz : method.getParameterTypes()) {
-                            if (Objects.equals(args[i].getClass().getName(), clazz.getName())) {
-                                c++;
-                            }
-                            i++;
-                            instance.getClass().getDeclaredMethod(methodName,)
-                        }
-                        if (Objects.equals(c, p)) {
+        for (Method method : instance.getClass().getDeclaredMethods()) {
+            if (method.getName().equals(name)) {
+                methodList.add(method);
+            }
+        }
 
-                        }
+        if (Objects.equals(methodList.size(), 0)) throw new RuntimeException("Method not found.");
+
+        if (Objects.nonNull(args) && args.length > 0) {
+            for (Method method : methodList) {
+                if (Objects.equals(method.getParameterCount(), args.length)) {
+                    if (Arrays.equals(args, method.getParameterTypes())) {
+                        m = method;
+                        break;
                     }
                 }
             }
-
-        } catch (Exception ex) {
-            // Caso ocorra alguma exceção do método chamado ou da reflexão
-        } catch (AssertionError ae) {
-            // Caso falhe a validação do step
-        } finally {
-            // Gerar evidencias
+        } else {
+            m = methodList.get(0);
         }
+        return m;
     }
 }
